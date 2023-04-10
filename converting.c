8e9d88074b3b4c3a26a31411b37d7a222db13e8c
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-10 13:30:30 -0400

    Tokenizing and convering code added
    description

--- converting.c
+++ converting.c
@@ -1,6 +1,20 @@
 #include "converting.h"
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 80
 
 void converting() {
 /* Version 2 */
-
+printf("*** Start of Converting Strings to double Demo ***\n");
+    char doubleString[BUFFER_SIZE];
+    double doubleNumber;
+    do{
+        printf("Type the double numeric string (q - to quit):\n");
+        fgets(doubleString, BUFFER_SIZE, stdin);
+        doubleString[strlen(doubleString) - 1] = '\0';
+        if ((strcmp(doubleString, "q") != 0)) {
+            doubleNumber = atof(doubleString);
+            printf("Converted number is %f\n", doubleNumber);
+        }
+    } while (strcmp(doubleString, "q")  != 0);
+    printf("*** End of Converting Strings to double Demo ***\n\n");
 }
