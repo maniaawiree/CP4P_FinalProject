b90223d3baa98606dd5155d9f53c24ac2ca4cf2d
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-09 20:46:28 -0400

    changes to version3

--- converting.c
+++ converting.c
@@ -1,16 +1,8 @@
 #include "converting.h"
 
 void converting() {
-/* Version 1 */
->> insert here
-
-
-/* Version 2 */
->> insert here
-
-
 /* Version 3 */
->> insert here
+
 
 
 }
--- tokenizing.c
+++ tokenizing.c
@@ -1,16 +1,8 @@
 #include "tokenizing.h"
 
 void tokenizing() {
-/* Version 1 */
->> insert here
-
-
-/* Version 2 */
->> insert here
-
-
 /* Version 3 */
->> insert here
+
 
 
 }
