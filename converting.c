64946c77eb9a7f1f768ae7a7e84df095edb665ee
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-09 20:13:48 -0400

    files added from local directory
    push to v1 branch

--- converting.c
+++ converting.c
@@ -1,16 +1,31 @@
 #include "converting.h"
+#define _CRT_SECURE_NO_WARNINGS
+#define BUFFER_SIZE 80
 
 void converting() {
 /* Version 1 */
->> insert here
 
-
+printf("*** Start of Converting Strings to int Demo ***\n");
+    char intString[BUFFER_SIZE];
+    int intNumber;
+do {
+    printf("Type an int numeric string (q - to quit):\n");
+    fgets(intString, BUFFER_SIZE, stdin); //gets and assigns the user input into intString,initializing the size of intString to BUFFER_SIZE; indicating it is a user input(via keyboard);
+    //fgets return intString as [char or int](any number of characters or ints)[\n][\0] 
+    intString[strlen(intString) - 1] = '\0'; //it sets the index number (length of the intString string - 1) from \n to NULL ;unexpected results can be avoided in subsequent concatenation operations
+    if (strcmp(intString, "q") != 0) { //it checks if intString and q are not identical;strcmp will return 0 if intString and q are identical.
+        intNumber = atoi(intString); //atoi returns 0 if there is no number at the beginning of the string, or return the integer string at the beginning of the string entered by user 75bhj -> 75; aaaa -> 0
+        printf("Converted number is %d\n", intNumber); //it outputs the first intergers entered by user
+        }
+    } while (strcmp(intString, "q") != 0); //the loop will end if user enters q, strcmp will return 0 if intString and q are identical.
+    printf("*** End of Converting Strings to int Demo ***\n\n");
 /* Version 2 */
->> insert here
+// >> insert here
 
 
 /* Version 3 */
->> insert here
+// >> insert here
 
 
 }
+
--- manipulating.c
+++ manipulating.c
@@ -1,16 +1,60 @@
 #include "manipulating.h"
-
+#define _CRT_SECURE_NO_WARNINGS  
+#define BUFFER_SIZE 80   
 void manipulating() {
 /* Version 1 */
->> insert here
+printf("*** start of Concatenating Strings Demo ***\n"); // Prints a message to start the concatenating strings demo.
+    // Declares two character arrays with a size of BUFFER_SIZE.
+    char string1[BUFFER_SIZE];
+    char string2[BUFFER_SIZE];
+    do {  // Loops until the user inputs "q" as the first string.
+        printf("Type the lst string (g - to quit): \n");  // Requests the first string from the user.
+        fgets(string1, BUFFER_SIZE, stdin);  // Reads the user's input and removes the newline character.
+        string1[strlen(string1) - 1] = '\0';
+        //  if the first string is not "q.", prompts the user to input the second string.
+        if ((strcmp(string1, "q") != 0))
+        {
+            printf("Type the 2nd string: \n");
+            // Reads the user's input for the second string and removes the newline character.
+            fgets(string2, BUFFER_SIZE, stdin);
+            string2[strlen(string2) - 1] = '\0';
+            // Concatenates the two strings and prints the result.
+            strcat(string1, string2);
+            printf("Concatenated string is \'%s\'\n", string1);
+        }
+    } while (strcmp(string1, "q") != 0);
+    // Prints a message to indicate the end of the concatenating strings demo.
+    printf("*** End of†Concatenating strings Demo ***\n\n");
 
 
 /* Version 2 */
->> insert here
+// >> insert here
 
 
 /* Version 3 */
->> insert here
+// >> insert here
 
 
 }
+
+
+
+
+// printf("*** Start of Concatenating String Demo ***\n");
+//    char string1[BUFFER_SIZE]; //sets the array size to the value of BUFFER_SIZE
+//    char string2[BUFFER_SIZE]; //sets the array size to the value of BUFFER_SIZE
+//
+//    do{
+//        printf("Type the 1st string (q - to quit):\n");
+//        fgets(string1, BUFFER_SIZE, stdin); //gets and assigns the user input into intString,initializing the size of string1 to BUFFER_SIZE; indicating it is a user input(via keyboard);
+//        //fgets return string1 as [char or int](any number of characters or ints)[\n][\0]
+//        string1[strlen(string1) - 1] = '\0'; //it sets the index number (length of the string1 - 1) from \n to NULL ;unexpected results can be avoided in subsequent concatenation operations
+//        if ((strcmp(string1, "q") != 0)){ //it checks if string1 and q are not identical;strcmp will return 0 if string1 and q are identical.
+//            printf("Type the 2nd string:\n");
+//            fgets(string2, BUFFER_SIZE, stdin); //the input will be stored in string2, buffersize number of elements in string, and stdin indicates it is entered by user(keyboard)
+//            string2[strlen(string2) - 1] = '\0';//it sets the index number (length of the string2 - 1) from \n to NULL ;unexpected results can be avoided in subsequent concatenation operations;
+//            strcat(string1, string2); //it concatenates string 1 and string 2, "string1string2"
+//            printf("Concatenated string is \'%s\'\n", string1); //outputs the concatenation result
+//        }
+//    }while(strcmp(string1, "q") != 0); //the loop will end if user enters q, strcmp will return 0 if string1 and q are identical.
+//    printf("*** End of Concatenating String Demo ***\n\n");
