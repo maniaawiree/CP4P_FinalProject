6644774eb7642585f395d2231ffba33695c81ca0
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-12 15:50:13 -0400

    modified

--- manipulating.c
+++ manipulating.c
@@ -4,34 +4,28 @@
 
 
 void manipulating() {
- printf("*** Start of Comparing Strings Demo ***\n"); 
+/* Version 2 */
+    printf("*** Start of Comparing Strings Demo ***\n"); 
 
-        char compare1[BUFFER_SIZE]; 
-        char compare2[BUFFER_SIZE]; 
-        int result; 
-
-        do 
-        {
-            printf("Type the Ist string to compare (q - to quit) : \n"); 
-            fgets(compare1, BUFFER_SIZE, stdin); 
-            compare1[strlen(compare1) - 1] = '\0'; 
-
-            if (strcmp(compare1, "q") != 0) 
-            {
-                printf("Type the 2nd string to compare: \n"); 
-                fgets(compare2, BUFFER_SIZE, stdin);
-                compare2[strlen(compare2) - 1] = '\0'; 
-
-                result = strcmp(compare1, compare2); 
-
-                if (result < 0) 
+    char compare1[BUFFER_SIZE]; 
+    char compare2[BUFFER_SIZE]; 
+    int result; 
+    do {
+        printf("Type the Ist string to compare (q - to quit) : \n"); 
+        fgets(compare1, BUFFER_SIZE, stdin); 
+        compare1[strlen(compare1) - 1] = '\0'; 
+        if ((strcmp(compare1, "q") != 0)) {
+            printf("Type the 2nd string to compare: \n"); 
+            fgets(compare2, BUFFER_SIZE, stdin);
+            compare2[strlen(compare2) - 1] = '\0';
+            result = strcmp(compare1, compare2); 
+            if      (result < 0) 
                     printf("\'%s\' string is less than \'%s\'\n", compare1, compare2); 
-                else if (result == 0) 
+            else if (result == 0) 
                     printf("\'%s\' string is equal to \'%s\'\n", compare1, compare2); 
-                else 
+            else 
                     printf("\'%s\' string is greater than \'%s\'\n", compare1, compare2); 
-            }
-        } while (strcmp(compare1, "q") != 0); 
-
-        printf("*** End of Comparing Strings Demo ***\n\n"); 
+        }
+    } while (strcmp(compare1, "q") != 0); 
+    printf("*** End of Comparing Strings Demo ***\n\n"); 
 }
