16bb018ed7f407c00b1dc54573b4440a74354a06
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-12 15:44:44 -0400

    all comments removed

--- tokenizing.c
+++ tokenizing.c
@@ -6,25 +6,20 @@ void tokenizing() {
 /* Version 2 */
 printf("*** Start of Tokenizing Phrases Demo ***\n");
     char phrases[BUFFER_SIZE]; 
-    char* nextPhrase = NULL; //declaration of a pointer to char variable initialized to 
+    char* nextPhrase = NULL;
     int phrasesCounter;
     do {
         printf("Type a few phrases seperated by comma(q - to quit):\n");
-        fgets(phrases, BUFFER_SIZE, stdin); /*fgets() is used to read the user's input 
-                                                    and store it in the phrases array. 
-                                                    The function reads up to BUFFER_SIZE - 1 characters from the standard input (stdin) 
-                                                    and stores them in phrases. If the user enters more characters than the size of 
-                                                    the array, the extra characters are discarded*/
-        phrases[strlen(phrases) - 1] = '\0'; //the newline character at the end of the user's input by is removed by replacing it with a null terminator.
-        if ((strcmp(phrases, "q") != 0)) { //if the user did not enter q to quit the function strtok will be called
-            nextPhrase = strtok(phrases, ","); /*the strtok() function is used to tokenize the input string using the comma as a delimiter; 
-                                                the first call to strtok() uses phrases array as an input */
+        fgets(phrases, BUFFER_SIZE, stdin); 
+        phrases[strlen(phrases) - 1] = '\0'; 
+        if ((strcmp(phrases, "q") != 0)) {
+            nextPhrase = strtok(phrases, ","); 
             phrasesCounter = 1; 
             while (nextPhrase) {
-                printf("Phrases #%d is \'%s\'\n", phrasesCounter++, nextPhrase); //phraseCounter is the number of the phrase and nextPhrase is the phrase extracted from the user input
-                nextPhrase = strtok(NULL, ","); //the subsequesnt calls use NULL so the strtok function continue using the same string
+                printf("Phrases #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+                nextPhrase = strtok(NULL, ","); 
             }
         }
-    } while (strcmp(phrases, "q") != 0); //The loop continues as long as the user does not enter "q" to quit.
+    } while (strcmp(phrases, "q") != 0);
     printf("*** End of Tokenizing Phrases Demo ***\n\n");
 }
