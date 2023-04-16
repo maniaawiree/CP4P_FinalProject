7c0da3ff4cf07cff04bae1a27d980a4747a7b48e
gyeongrok <0kr3670@gmail.com>
2023-04-16 12:12:56 +0900

    code added

--- fundamentals.c
+++ fundamentals.c
@@ -4,16 +4,27 @@
 
 void fundamentals(){
 /* Version 2 */
-    printf("*** Start of Measuring strings Demo ***\n");
+    printf("*** Start of Measuring Strings Demo ***\n");
+
+
     char buffer2[BUFFER_SIZE];
-    do { 
-        printf("Type a string (q - to quit):\n"); 
-        fgets(buffer2, BUFFER_SIZE, stdin); 
-        buffer2[strlen(buffer2) - 1] = '0'; 
-        if (strcmp (buffer2, "q") != 0)
-            printf ("The length of \'%s\' is %d characters\n",
-                     buffer2, (int)strlen(buffer2));
-    } while (strcmp(buffer2, "q") != 0) ; 
-   printf ("*** End of Measuring Strinas Demo ***\n\n");
+
+    do {
+        printf("Type a string (q - to quit):\n");
+        
+
+        fgets(buffer2, BUFFER_SIZE, stdin);
+        
+        buffer2[strlen(buffer2) - 1] = '\0';
+        
+
+        if (strcmp(buffer2, "q") != 0) {
+            
+            printf("The length of \'%s\' is %d characters\n", buffer2, (int)strlen(buffer2));
+        }
+    } while (strcmp(buffer2, "q") != 0);
+
+    printf("*** End of Measuring Strings Demo ***\n\n");
+
 
 }
