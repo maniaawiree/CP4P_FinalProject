aa955ff51633124a6690b05504b8dfbb90025fdc
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-17 21:42:39 -0400

    Gyeongrok's comments

--- fundamentals.c
+++ fundamentals.c
@@ -7,23 +7,41 @@
 
 void fundamentals(){
 /* Version 3 */
-    printf("*** Start of Copying Strings Demo ***");
+    printf("*** Start of copying strings Demo **\n");
+    // Print a message to indicate the start of the program
+
     char destination[BUFFER_SIZE];
+    // Declare a character array called destination with a size of BUFFER_SIZE
+
     char source[BUFFER_SIZE];
+    // Declare a character array called source with a size of BUFFER_SIZE
+
     do {
         destination[0] = '\0';
-        printf("Destinstion string is reset to empty\n");
+        // Set the first character of the destination array to a null terminator to reset the array to an empty string
+        printf("Destination string is reset to empty\n");
+        // Print a message to indicate that the destination string has been reset to an empty string
+        printf("Type the source string (q - to quit);\n");
+        // Prompt the user to input a string
+
         fgets(source, BUFFER_SIZE, stdin);
+        // When the program reaches the fgets() function call, it waits for the user to input a string of characters and press enter. Once the user presses enter, fgets() reads the input from stdin and stores it in the source array. If the user enters more than BUFFER_SIZE - 1 characters, fgets() will only read up to BUFFER_SIZE - 1 characters and store them in the source array.
+
         source[strlen(source) - 1] = '\0';
-        if (strcmp(destination, source) != 0) {
+        // Remove the newline character from the input
+
+        if (strcmp(source, "q") != 0) {
+            // The line if (strcmp(source, "q")!=0) checks that the user input stored in source is not equal to the string "q" (zero if source is q). If the input is not "q", the strcpy() function is used to copy the string stored in source to the destination array. The new value of destination is then printed using printf().
             strcpy(destination, source);
             printf("New destination string is \'%s\'\n", destination);
         }
+
     } while (strcmp(source, "q") != 0);
-    printf("*** End of Copying Strings Demo ***\n\n");
-
-
+    // Repeat the loop until the user enters "q"
+    printf("*** End of Copying strings Demo ***\n\n");
+    // Print a message to indicate the end of the program
 }
+
 
 
 
