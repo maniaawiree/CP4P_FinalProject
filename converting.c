72bc188ea9fc119d722e2eb6f461b2c1a4b78d59
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-17 21:47:04 -0400

    working

--- converting.c
+++ converting.c
@@ -9,7 +9,11 @@ void converting() {
     long longNumber;
     do {
         printf("Type the long numeric string (q - to quit):\n");
-        fgets(longString, BUFFER_SIZE, stdin); 
+        fgets(longString, BUFFER_SIZE, stdin); /*fgets() is used to read the user's input 
+                                                    and store it in the longString array. 
+                                                    The function reads up to BUFFER_SIZE - 1 characters from the standard input (stdin) 
+                                                    and stores them in longString. If the user enters more characters than the size of 
+                                                    the array, the extra characters are discarded*/
         longString[strlen(longString)] = '\0';
         if (strcmp(longString, "q") != 0) {
             longNumber = atol(longString);
