4383e241b27f8a5f23a12d5a5ae27b108cd9a23d
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-17 21:45:55 -0400

    code added

--- converting.c
+++ converting.c
@@ -4,5 +4,18 @@
 
 void converting() {
 /* Version 3 */
+    printf("*** Start of Converting Strings to long Demo ***\n");
+    char longString[BUFFER_SIZE];
+    long longNumber;
+    do {
+        printf("Type the long numeric string (q - to quit):\n");
+        fgets(longString, BUFFER_SIZE, stdin); 
+        longString[strlen(longString)] = '\0';
+        if (strcmp(longString, "q") != 0) {
+            longNumber = atol(longString);
+            printf("Converted number is %ld\n", longNumber);
+        }
+    } while (strcmp (longString, "q") != 0);
+    printf("*** End of Converting Strings to long Demo ***\n\n");
 }
 
