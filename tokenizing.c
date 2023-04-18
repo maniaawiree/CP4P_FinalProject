25e9eb4463fbf7506f0a4ec4e43430a3a660b257
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-17 22:08:43 -0400

    comments added

--- tokenizing.c
+++ tokenizing.c
@@ -17,17 +17,15 @@ void tokenizing() {
                                                     the array, the extra characters are discarded*/
         sentences[strlen(sentences) - 1] = '\0'; //the newline character at the end of the user's input by is removed by replacing it with a null terminator.
         if ((strcmp(sentences, "q") != 0)) { //if the user did not enter q to quit the function atol will be called
-            nextSentence = strtok(sentences, "."); /*strtok is called on the input string using "." as the delimiter 
-                                                        to tokenize the input. It sets nextSentence 
-                                                        to the first token */
+            nextSentence = strtok(sentences, "."); /*the strtok() function is used to tokenize the input string using period(dot) as a delimiter; 
+                                                the first call to strtok() uses sentences array as an input */   
             sentenceCounter = 1; //sentenceCounter is set to 1.
             while (nextSentence) { //while loop iterates as long as nextSentence is not NULL
-                printf("Sentence #%d is \'%s\'\n", sentenceCounter++, nextSentence); //the sentence number and the sentence(token)itself is printed to the console
+                printf("Sentence #%d is \'%s\'\n", sentenceCounter++, nextSentence); //sentenceCounter is the number of the sentence and nextSentence is the sentence extracted from the user input
                                                                                     //sentenceCounter is increased by 1
-                nextSentence = strtok(NULL, "."); //nextSentence is set to the next token by calling strtok with NULL as the first argument and dot as delimiter.
+                nextSentence = strtok(NULL, "."); //the subsequesnt calls use NULL so the strtok function continue using the same string
             }
         }
-    } while (strcmp(sentences, "q") != 0);
+    } while (strcmp(sentences, "q") != 0); //The loop continues as long as the user does not enter "q" to quit.
     printf("*** End of Tokenizing Sentences Demo ***\n\n");
-
 }
