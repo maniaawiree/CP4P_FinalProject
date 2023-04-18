757b3abe42b1bd8227901836c936eab019da9b66
mmojdehifar <mmojdehifar@myseneca.ca>
2023-04-17 21:11:19 -0400

    code added

--- fundamentals.c
+++ fundamentals.c
@@ -1,7 +1,21 @@
 #include "fundamentals.h"
+#define _CRT_SECURE_NO_WARNINGS  
+#define BUFFER_SIZE 80  
 
 void fundamentals() {
 /* Version 3 */
-
-
+    printf("*** Start of Copying Strings Demo ***");
+    char destination[BUFFER_SIZE];
+    char source[BUFFER_SIZE];
+    do {
+        destination[0] = '\0';
+        printf("Destinstion string is reset to empty\n");
+        fgets(source, BUFFER_SIZE, stdin);
+        source[strlen(source) - 1] = '\0';
+        if (strcmp(destination, source) != 0) {
+            strcpy(destination, source);
+            printf("New destination string is \'%s\'\n", destination);
+        }
+    } while (strcmp(source, "q") != 0);
+    printf("*** End of Copying Strings Demo ***\n\n");
 }
